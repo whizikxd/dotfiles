706c82545cb8f8565fa41d88c67c860b3ebbb884
Timotej Milčák <timotejmk94@gmail.com>
2024-06-19 11:26:20 +0000

    Initial commit

---
+++ .bashrc
@@ -0,0 +1,14 @@
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto --group-directories-first'
+alias grep='grep --color=auto'
+
+# Old default prompt
+#PS1='[\u@\h \W]\$ '
+
+# Custom stuff
+PS1='\[\e[38;5;244;1m\]\u\[\e[97m\]@\[\e[38;5;244m\]\h\[\e[39m\][\[\e[97m\]\w\[\e[39m\]]\n$? > \[\e[0m\]'
+PS2='\n> \[\e[0m\]'
+
+alias e='nvim'
---
+++ .config/nvim/init.lua
@@ -0,0 +1,47 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.colorcolumn = "79" 
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.history = 150
+
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 20
+vim.g.netrw_keepdir = 0
+vim.g.netrw_sort_sequence = [[[\/]$,*]]
+vim.g.netrw_sizestyle = "H"
+-- vim.g.netrw_browse_split = 2
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {"ellisonleao/gruvbox.nvim", lazy = false, config = true, opts = ...},
+})
+
+vim.opt.background = "dark"
+vim.cmd("colorscheme gruvbox")
