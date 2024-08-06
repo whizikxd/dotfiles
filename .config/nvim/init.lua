18e9ba3ae08b742b9591add9ecf48444cb444bae
Timotej Milcak <timotejmk94@gmail.com>
2024-08-06 20:26:02 +0200

    oil.nvim configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -159,7 +159,35 @@ require("lazy").setup({
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local oil = require("oil")
-      oil.setup()
+      oil.setup({
+        default_file_explorer = true,
+        constrain_cursor = "name",
+        delete_to_trash = false,
+        skip_confirm_for_simple_edits = true,
+        columns = {
+          "icon",
+          "permissions",
+          "size"
+        },
+        keymaps = {
+          ["g?"]    = "actions.show_help",
+          ["<CR>"]  = "actions.select",
+          ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+          ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+          ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"]     = "actions.parent",
+          ["_"]     = "actions.open_cwd",
+          ["`"]     = "actions.cd",
+          ["~"]     = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+          ["gs"]    = "actions.change_sort",
+          ["gx"]    = "actions.open_external",
+          ["g."]    = "actions.toggle_hidden",
+          ["g\\"]   = "actions.toggle_trash"
+        }
+      })
 
       vim.keymap.set("n", "<leader>vo", ":Oil<CR>", {silent = true})
     end,
