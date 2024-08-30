d4bf6932f606364d967c3b73058c1f45cc47063c
Timotej Milcak <timotejmk94@gmail.com>
2024-08-30 14:45:13 +0200

    Fix telescope branch name

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
   lazy = false,
-  branch = "0.1.8",
+  branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons"
