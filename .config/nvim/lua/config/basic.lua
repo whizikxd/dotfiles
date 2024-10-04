29ceb019af26019d961e5b73f8c2fab714877c4c
Timotej Milčák <timotejmk94@gmail.com>
2024-10-04 20:29:44 +0200

    update basic.lua

--- .config/nvim/lua/config/basic.lua
+++ .config/nvim/lua/config/basic.lua
@@ -9,12 +9,12 @@ function setup()
   vim.opt.relativenumber = true
   vim.opt.ignorecase     = true
   vim.opt.smartcase      = true
-  vim.opt.hlsearch       = false
+  vim.opt.hlsearch       = true
   vim.opt.wrap           = true
   vim.opt.breakindent    = true
-  vim.opt.tabstop        = 2
-  vim.opt.shiftwidth     = 2
-  vim.opt.expandtab      = true
+  vim.opt.tabstop        = 8
+  vim.opt.shiftwidth     = 8
+  vim.opt.expandtab      = false
   vim.opt.colorcolumn    = "79"
   vim.opt.clipboard      = "unnamedplus"
   vim.opt.history        = 150
@@ -25,7 +25,8 @@ function setup()
   vim.opt.cursorline     = true
   vim.opt.laststatus     = 0
   vim.opt.listchars      = {
-    tab = "->",
+    tab = "  ",
+    lead = "~",
     trail = "~",
   }
 end
