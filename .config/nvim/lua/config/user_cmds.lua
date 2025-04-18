6a684d811da29c6ad31bdcc051e01ee1fef50393
Timotej Milcak <timotejmk94@gmail.com>
2025-04-18 13:53:10 +0200

    some more uppercase save commands

--- .config/nvim/lua/config/user_cmds.lua
+++ .config/nvim/lua/config/user_cmds.lua
@@ -2,6 +2,8 @@ function setup()
   vim.api.nvim_create_user_command('W', 'w', {})
   vim.api.nvim_create_user_command('Wq', 'wq', {})
   vim.api.nvim_create_user_command('WQ', 'wq', {})
+  vim.api.nvim_create_user_command('Wa', 'wa', {})
+  vim.api.nvim_create_user_command('WA', 'wa', {})
 end
 
 return {
