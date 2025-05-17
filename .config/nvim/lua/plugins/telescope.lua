b531d4069c94e817def912960a691de58feea86c
Timotej Milcak <timotejmk94@gmail.com>
2025-05-17 12:24:40 +0200

    dont ff on pull + more precise exclusion of .git/

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
         find_files = {
           hidden = true,
           file_ignore_patterns = {
-            ".git/"
+            "^%.git/"
           }
         },
         live_grep = {
